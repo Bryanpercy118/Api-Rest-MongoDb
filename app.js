725bfc5456855b6804a91b694f60b9ce06efcde9
bryanpercy118 <bryanpercy77@gmail.com>
2024-04-23 14:40:18 -0500

    Method Get findById  Complete With MongoDb

--- app.js
+++ app.js
@@ -22,6 +22,14 @@ router.get("/", async(req, res) => {
     res.send(respuesta);
 })
 
+
+router.get("/:id", async(req, res) => {
+    const id = req.params.id;
+    const respuesta = await ModelUser.findById(id);
+    res.send(respuesta);
+})
+
+
 app.use(express.json())
 app.use(router)
 app.listen(3001, () => {
